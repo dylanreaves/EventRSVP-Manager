ec6a7b6fec28b25a759e6634418c854137a33b11
Fausto Vazquez <faustovazquez.005@gmail.com>
2026-06-16 14:28:59 -0400

    Part 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,50 @@ import './App.css'
 export default function App() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [guests, setGuest] = useState([])
   // function updateName (event) {
   //   setName(event.target.value)
   // }
+  function handleSumbit(e) {
+
+    e.preventDefault()
+    const newGuest = {
+      id: Date.now(),
+      name: e.target.name.value,
+      email: e.target.email.value
+    }
+    setGuest([...guests, newGuest])
+    e.target.name.value = ''
+    e.target.email.value =''
+
+  }
+
+
 
   return (
     <div>
       <h1>Company Picnic RSVP</h1>
-      <form>
-        <label for="name"> Name </label>
-        <input type="name" name="name"></input>
+      <form onSubmit={handleSumbit}>
+        <label> Name </label>
+        <input onChange={(e)=> setName(e.target.value)} type="name" name="name"></input>
         <br/>
-        <label for="email"> Email </label>
-        <input type="email" name="email"></input>
+        <label> Email </label>
+        <input onChange={(e)=> setEmail(e.target.value)} type="email" name="email"></input>
         <br/>
         <button type="submit"> Add Guest </button>
       </form>
       <p> {name} </p>
       <p> {email} </p>
+      <br/>
+      <h2>Guest List: </h2>
+      <ul>
+        {guests.map((guest)=>{
+          return (
+            <li>Name: {guest.name} -- Email:{guest.email}</li>
+          )
+        })}
+      </ul>
+      
     </div>
   )
 }
