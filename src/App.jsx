310dbc7c1fcfd80be6a0ee99615921b06d3578fe
dylan reaves <dylanmilesreaves@gmail.com>
2026-06-16 13:53:59 -0400

    part 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import './App.css'
 
 export default function App() {
   const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
   // function updateName (event) {
   //   setName(event.target.value)
   // }
+
   return (
     <div>
-      <h1>RSVP Practice</h1>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      <p>{name}</p>
+      <h1>Company Picnic RSVP</h1>
+      <form>
+        <label for="name"> Name </label>
+        <input type="name" name="name"></input>
+        <br/>
+        <label for="email"> Email </label>
+        <input type="email" name="email"></input>
+        <br/>
+        <button type="submit"> Add Guest </button>
+      </form>
+      <p> {name} </p>
+      <p> {email} </p>
     </div>
   )
 }
